9bda8ba568d4947d5f15a1416b936f2a660babcb
Mahdi <mkfard@outlook.com>
2024-12-30 19:14:24 +0330

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"6a75cb6a6a57ae5a05309a43f0fba6cd50c657
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3717139508"
+    serviceWorkerVersion: "1683682775"
   }
 });
