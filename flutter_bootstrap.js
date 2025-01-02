65949fcf9cbd49befc842244343250e24282550c
Mahdi <mkfard@outlook.com>
2025-01-02 14:31:10 +0330

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,7 +6,7 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"6a75cb6a6a57ae5a05309a43f0fba6cd50c65752","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"d9b7e5664604a55a3e900b0c619ee319065abb0e","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 const config = { canvasKitBaseUrl: '/canvaskit/' };
