5ac2b3256b9590f9311d42a334859386a74f85fb
Mahdi <mkfard@outlook.com>
2025-06-14 22:16:49 +0330

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d18c2640e346152390bc4580ab88abe1
 "icons/Icon-maskable-512.png": "09d31f8359c490a24a9ba012434e3937",
 "index.html": "2b9ca49c6f559f7beecfa03bfec591e3",
 "/": "2b9ca49c6f559f7beecfa03bfec591e3",
-"main.dart.js": "14d0906892451382cbdc4ed3a4e1a70a",
+"main.dart.js": "f78fc729c32dd13bf6d2a026055cd7e5",
 "manifest.json": "9de07508b5049e6206ca38d6f73f5b5d",
 "telegram-web-app.js": "2db55d684f07c95beee17f42802f772b",
 "version.json": "20b39d34b01de1d6b9d4105d01e38cf1"};
