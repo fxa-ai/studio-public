915ac67fb9ca62ac77b623af6a25ac85d85a0e6d
Mahdi <mkfard@outlook.com>
2025-06-14 18:37:13 +0330

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d18c2640e346152390bc4580ab88abe1
 "icons/Icon-maskable-512.png": "09d31f8359c490a24a9ba012434e3937",
 "index.html": "1b48e076ee41e7a57e07f5a041adb831",
 "/": "1b48e076ee41e7a57e07f5a041adb831",
-"main.dart.js": "f844009fe27ff7d1a8d04e6853631e71",
+"main.dart.js": "8fdaa21c65dbbdcaf7770a3a8a0483d7",
 "manifest.json": "9de07508b5049e6206ca38d6f73f5b5d",
 "version.json": "20b39d34b01de1d6b9d4105d01e38cf1"};
 // The application shell files that are downloaded before a service worker can
